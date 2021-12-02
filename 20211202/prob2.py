b57b7b68b9e022900137c25d2469f0a7a4cf4968
mchartigan <hartigan@purdue.edu>
2021-12-02 00:07:03 -0500

    day 2 finished!! beat nicky

--- 20211202/prob2.py
+++ 20211202/prob2.py
@@ -1,5 +1,6 @@
 fwd = 0
 depth = 0
+aim = 0
 
 with open("input.txt") as file:
     lines = file.readlines()
@@ -8,9 +9,10 @@ with open("input.txt") as file:
         list = line.split(' ')
         if list[0] == "forward":
             fwd = fwd + int(list[1])
+            depth = depth + aim * int(list[1])
         elif list[0] == "down":
-            depth = depth + int(list[1])
+            aim = aim + int(list[1])
         elif list[0] == "up":
-            depth = depth - int(list[1])
+            aim = aim - int(list[1])
 
     print(depth*fwd)
